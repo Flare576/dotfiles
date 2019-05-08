a9e496cfbf80d0a26c44f806153ccc33717905c6
Flare576 <flare576@gmail.com>
2019-05-08 10:23:33 -0500

    Fixing npm autocomplete

--- scripts/setupOmz.sh
+++ scripts/setupOmz.sh
@@ -4,6 +4,9 @@ pip install --user powerline-status
 curl -sL http://install.ohmyz.sh | sh
 cd $HOME/.oh-my-zsh/themes
 curl -LSso cobalt2.zsh-theme https://raw.githubusercontent.com/wesbos/Cobalt2-iterm/master/cobalt2.zsh-theme
+cd $HOME/.oh-my-zsh/custom/plugins/
+git clone git clone https://github.com/lukechilds/zsh-better-npm-completion
+
 cd /tmp
 git clone https://github.com/powerline/fonts
 cd fonts
