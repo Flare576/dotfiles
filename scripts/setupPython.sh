c1a015827ecb3889152ffa292c32983efd561022
Flare576 <flare576@gmail.com>
2019-06-20 09:33:45 -0500

    maybe this will work, not sure if export persists in script

--- scripts/setupPython.sh
+++ scripts/setupPython.sh
@@ -2,7 +2,10 @@
 echo "Setting up Python"
 
 # Install Python and applications
+eval "$(pyenv init -)"
 pyenv install 3.7.2
 pyenv global 3.7.2
-eval "$(pyenv init -)"
+
+
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 pip install cheat
