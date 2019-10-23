5503ff44e3da408e8e28cdd90f8fd43dd989b9d1
Flare576 <flare576@gmail.com>
2019-10-23 08:46:19 -0500

    doh

--- scripts/setupHomebrew.sh
+++ scripts/setupHomebrew.sh
@@ -23,8 +23,7 @@ brews=(
 )
 
 if [ "$isLinux" -eq "1" ] ; then
-  touch $HOME/.doNotCommit
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> $HOME/.doNotCommit
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> $HOME/dotfiles/.doNotCommit
 else
   brews+=(cask git kubectx mas)
 fi
