66f256d540fc77724352dab8bee32e192ca0586f
flare576 <flare576@gmail.com>
2019-10-25 09:19:01 -0500

    Should let me push now

--- scripts/setupRepo.sh
+++ scripts/setupRepo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Cloning DotFiles repo"
+echo "Securing DotFiles repo"
 
 cd $HOME/dotfiles
 git secrets --install
 git secrets --register-aws
+
+echo "If dotfiles was cloned https, convert to ssh"
+sed -i'' -e 's/https:\/\/github.com\//git@github.com:/' $HOME/dotfiles/.git/config
