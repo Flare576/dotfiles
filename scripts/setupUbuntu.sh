c7b0594deedd010bd4e8dec3c61e5445b2c8fbb2
Jeremy Scherer <flare576@gmail.com>
2020-05-31 18:57:31 -0500

    Update setupUbuntu.sh

--- scripts/setupUbuntu.sh
+++ scripts/setupUbuntu.sh
@@ -10,7 +10,6 @@ if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-  brew install gcc
 fi
 # Pull the rest of the project
 cd $HOME
