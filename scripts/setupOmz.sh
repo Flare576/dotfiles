5b82cf30368c60863cd3ffee33826486fcff0eb0
Flare576 <flare576@gmail.com>
2020-06-09 19:54:19 -0500

    Leave omz alone!

--- scripts/setupOmz.sh
+++ scripts/setupOmz.sh
@@ -3,23 +3,6 @@ isLinux=0; [ -f "/etc/os-release" ] && isLinux=1
 echo "Setup Oh My Zshell"
 
 # Install and setup Oh My Zshell
-if [ -d "/mnt/c" ] ; then
-  read -p "Are you running in Windows Submodule for Linux (WSL)? (Y/n)" inwsl
-  if [[ "$inwsl" =~ ^[yY] ]] ; then
-    echo "You'll need to install Powerline fonts for Windows. Open Powershell **AS ADMINISTRATOR**, and copy-pasta:
-    https://raw.githubusercontent.com/powerline/fonts/master/Inconsolata/Inconsolata%20for%20Powerline.otf
-
-    powershell -command \"& { iwr https://github.com/powerline/fonts/archive/master.zip -OutFile ~\fonts.zip }\"
-    Expand-Archive -Path ~\\fonts.zip -DestinationPath ~
-    Set-ExecutionPolicy Bypass
-    ~\\fonts-master\install.ps1
-    Set-ExecutionPolicy Default
-    rm -r ~\\fonts-master
-    rm ~\\fonts.zip
-
-    Then, click the Ubuntu logo in the top-left, choose Properties, and set the Font to 'DejaVu Sans Mono for Powerline'."
-  fi
-fi
 pip3 install --user powerline-status
 curl -sL http://install.ohmyz.sh | sh
 cd $HOME/.oh-my-zsh/themes
