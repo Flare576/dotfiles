1593b9ae720b6281ec09337e38008e998660b376
Flare576 <flare576@gmail.com>
2020-10-14 12:15:27 -0500

    update brew install

--- scripts/setupMac.sh
+++ scripts/setupMac.sh
@@ -5,7 +5,7 @@
 # Install Homebrew
 if test ! $(which brew); then
   echo "Installing Homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo "Instalalling git for project clone"
