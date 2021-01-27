e81c2d087f38f3fbb43d2bb0bb0c905dfcdfb6b5
flare576 <flare576@gmail.com>
2021-01-27 12:29:50 -0600

    feat(vim): setup allows syntax selection
    Vim plugins are getting a bit extensive, adding a filter for
    language/syntax

--- scripts/setupVim.sh
+++ scripts/setupVim.sh
@@ -7,6 +7,33 @@ plugins=~/.vim/pack/plugins/start/
 mkdir -p $plugins
 cd $plugins
 
+read -p "Minimal Vim? (y/N)" miniV
+if [[ $miniV == "y"* ]] ; then
+  exit
+fi
+
+
+function checkAll() {
+  if [[ "$includeJS $includeTS $includePython $includeWriting" == *"a"* ]] ; then
+    includeJS='y'
+    includeTS='y'
+    includePython='y'
+    includeWriting='y'
+  fi
+}
+
+
+echo "Syntax Plugin Options:"
+[ -z "$includeJS" ] && read -p "Include javascript? (y)es/(n)o/(a)ll: " includeJS
+checkAll
+[ -z "$includeTS" ] && read -p "Include typescript?  (y)es/(n)o/(a)ll: " includeTS
+checkAll
+[ -z "$includePython" ] && read -p "Include python? (y)es/(n)o/(a)ll: " includePython
+checkAll
+[ -z "$includeWriting" ] && read -p "Include writing tools? (y)es/(n)o/(a)ll: " includeWriting
+checkAll
+
+echo "Installing vim plugins"
 ############################## Theme ###########
 # Preferred Theme
 git clone https://github.com/altercation/vim-colors-solarized.git
@@ -30,7 +57,7 @@ git clone https://github.com/lingceng/z.vim.git
 # Flip those surroundings!
 git clone https://github.com/tpope/vim-surround.git
 # Multi-cursor support
-git clone https://github.com/terryma/vim-multiple-cursors.git
+# git clone https://github.com/terryma/vim-multiple-cursors.git
 # Better undotree
 git clone https://github.com/mbbill/undotree.git
 # Merge tool soooo good
@@ -39,22 +66,31 @@ git clone https://github.com/samoshkin/vim-mergetool.git
 git clone https://github.com/ludovicchabant/vim-gutentags.git
 git clone https://github.com/majutsushi/tagbar.git
 # EditorConfig support (see https://editorconfig.org/)
-git clone https://github.com/editorconfig/editorconfig-vim.git
+# git clone https://github.com/editorconfig/editorconfig-vim.git
 
-############################## Syntax ###########
-# JSX
-git clone https://github.com/MaxMEllon/vim-jsx-pretty.git
-# JSON
-git clone https://github.com/elzr/vim-json.git
-# Javascript
-git clone https://github.com/pangloss/vim-javascript.git
-# HTML
-git clone https://github.com/vim-scripts/indenthtml.vim.git
-# Typescript
-git clone https://github.com/leafgarland/typescript-vim.git
-# Thesourus... does English count as syntax? ¯\_(ツ)_/¯ 
-echo "Downloading thesaurus..."
-thesDir="$XDG_DATA_HOME"
-[ -z "$thesDir" ] && thesDir="$HOME/.local/share"
-mkdir -p "$thesDir"
-curl -sL -o "$thesDir/thesaurus.txt" http://www.gutenberg.org/files/3202/files/mthesaur.txt
+
+if [[ $includeJS == "y"* ]] ; then
+  # JSX
+  git clone https://github.com/MaxMEllon/vim-jsx-pretty.git
+  # JSON
+  git clone https://github.com/elzr/vim-json.git
+  # Javascript
+  git clone https://github.com/pangloss/vim-javascript.git
+fi
+if [[ $includeTS == "y"* ]] ; then
+  # Typescript
+  git clone https://github.com/leafgarland/typescript-vim.git
+fi
+if [[ $includeWriting == "y"* ]] ; then
+  echo "Downloading thesaurus..."
+  thesDir="$XDG_DATA_HOME"
+  [ -z "$thesDir" ] && thesDir="$HOME/.local/share"
+  mkdir -p "$thesDir"
+  curl -sL -o "$thesDir/thesaurus.txt" http://www.gutenberg.org/files/3202/files/mthesaur.txt
+fi
+if [[ $includePython == "y"* ]] ; then
+  # Intelligent indentation handling
+  git clone https://github.com/vim-scripts/indentpython.vim.git
+  # Autocompletion for VIM
+  git clone https://github.com/davidhalter/jedi-vim.git
+fi
