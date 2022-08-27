c2be2d146c881db11be0fb839965c41a5719da05
flare576 <flare576@gmail.com>
2022-08-26 23:50:48 -0500

    fix(osx): remove wrong code for clock

--- setup/OSX/menuClock.sh
+++ setup/OSX/menuClock.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 # See https://www.tech-otaku.com/mac/setting-the-date-and-time-format-for-the-macos-menu-bar-clock-using-terminal/
+# full script https://github.com/tech-otaku/menu-bar-clock/blob/main/menu-bar-clock.sh
 # should be com.apple.menuextra.clock.plist,
 
-defaults write com.apple.Terminal "Window Settings" -dict-add Sweetness \
-  '<dict>
-    <key>DateFormat</key>
-    <string>EEE MMM d  h:mm a</string>
-    <key>FlashDateSeparators</key>
-    <false/>
-    <key>IsAnalog</key>
-    <false/>
-  </dict>'
