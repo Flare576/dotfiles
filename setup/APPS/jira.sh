22314f34266b99cc4bc20f59848ec8e35bc901e2
flare576 <flare576@gmail.com>
2022-12-01 16:40:46 -0600

    refactor(jira): standardize jira install

--- setup/APPS/jira.sh
+++ setup/APPS/jira.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
 # https://github.com/Netflix-Skunkworks/go-jira
+source "$(dirname "$0")/../utils.sh"
+communityDir=$HOME/dotfiles/cheat/community
 
-if [[ "$1" == "delete" ]]; then
+usage="$(basename "$0") [-hvd]
+Links Cheat configs and Installs/Upgrades Cheat and community cheat sheets.
+Cheat is a tool that prints out notes you've taken (or borrowed) about commands, tools, etc. - try 'cheat tar'
+  -h Show this help
+  -v Display version
+  -d Uninstall cheat
+"
+while getopts ':hvd' option; do
+  case "$option" in
+    h) echo "$usage"
+      exit
+      ;;
+    v) echo "$VERSION"
+      exit
+      ;;
+    d) doDestroy="true"
+      ;;
+    *) echo "Unknown Option '$option', exiting"
+      exit
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ "$doDestroy" == "true" ]; then
   rm -rf "$HOME/.jira.d"
+  if ! dotRemove flare576/scripts/jira-cli "manual"; then
+    rm /usr/local/bin/jira
+  fi
   exit
 fi
-ln -sf "$HOME/dotfiles/.jira.d" "$HOME"
-brew install flare576/scripts/jira-cli
+
 read -p "Are you actively working on JIRA instance? (Y/n)" doit
-echo
 
 if [[ $doit =~ ^[yY] ]] ; then
+  ln -sf "$HOME/dotfiles/.jira.d" "$HOME"
+  if ! dotInstall flare576/scripts/jira-cli "manual"; then
+    DLURL=$(latestGit "go-jira/jira" "jira-linux-amd64")
+    curl -sL ${DLURL} -o /tmp/jira-linux-amd64 \
+    && chmod +x /tmp/jira-linux-amd64 \
+    && mv /tmp/jira-linux-amd64 /usr/local/bin/jira
+  fi
+  echo
   echo "Setting up Jira CLI"
   read -p "What is your email address? " email
   read -p "What is your base Jira URL? " url
