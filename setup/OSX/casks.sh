56d6a82c194c5615e7ed7786fb7ae83c5651f6f7
flare576 <flare576@gmail.com>
2025-09-04 19:48:32 -0500

    feat(casks): remove azure, add extensions

--- setup/OSX/casks.sh
+++ setup/OSX/casks.sh
@@ -2,7 +2,6 @@
 casks=(
   1password          # Password Manager!
   1password-cli      # Still Password Manger... ON CLI!
-  azure-data-studio  # Great Database client
   boop               # Small app to let you do quick text/json/etc. tweeks
   firefox            # I mean, why not!
   google-chrome      # Like you don't know!
@@ -17,12 +16,33 @@ casks=(
   zoom               # You know you're going to need it
 )
 
+code_exts=(
+  alexcvzz.vscode-sqlite
+  ckolkman.vscode-postgres
+  jebbs.plantuml
+  vscodevim.vim
+)
+
 echo "Installing Homebrew Casks"
 for cask in "${casks[@]}"
 do
   echo "Working on $cask"
   brew install --cask "$cask"
 done
+
+echo "Install VS Code Extensions"
+for extension in "${code_exts[@]}"
+do
+  echo "Working on $extension"
+  code --install-extension "$extension"
+done
+filename="$HOME/Library/Application Support/Code/User/settings.json"
+# Read existing content or provide an empty JSON object if the file doesn't exist.
+# Then update the 'workbench.colorTheme' using jq and write back atomically.
+# The '2>/dev/null' suppresses potential error messages from 'cat' if the file is not found initially.
+( [ -f "$filename" ] && cat "$filename" 2>/dev/null || echo "{}" ) | \
+jq --arg theme "$code_theme" '.{"vim.vimrc.enable": true,"vim.vimrc.path":"~/.vimrc"}' > "$filename.tmp" && \
+mv "$filename.tmp" "$filename"
 
 # Force setup of apps
 open -a "1Password"
@@ -35,3 +55,4 @@ open -a Slack
 read -p "Once you setup Slack, press enter."
 open -a LaunchBar
 read -p "Once you setup LaunchBar, press enter."
+
